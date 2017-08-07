178131f2b4ad97eb4b619640e11ee35358af5b78
unknown <3203317@qq.com>
2017-08-07 20:50:38 +0800

    haha

--- logon/src/biz/gift.js
+++ logon/src/biz/gift.js
@@ -25,7 +25,16 @@ const biz = require('emag.biz');
 const logger = require('log4js').getLogger('biz.gift');
 
 (() => {
-  var sql = 'SELECT d.type_name gift_type_name, c.goods_name, b.user_name, a.* FROM (SELECT * FROM w_gift WHERE user_id=?) a LEFT JOIN s_user b ON (a.user_id=b.id) LEFT JOIN w_goods c ON (a.goods_id=c.id) LEFT JOIN w_gift_type d ON (a.gift_type=d.id) WHERE b.id IS NOT NULL AND c.id IS NOT NULL AND d.id IS NOT NULL ORDER BY a.create_time DESC';
+  var sql = 'SELECT '+
+              'e.prop_name, d.type_name gift_type_name, c.goods_name, b.user_name, b.vip user_vip, a.* '+
+                'FROM (SELECT * FROM w_gift WHERE user_id=?) a '+
+                  'LEFT JOIN s_user b ON (a.user_id=b.id) '+
+                  'LEFT JOIN w_goods c ON (a.goods_id=c.id) '+
+                  'LEFT JOIN w_gift_type d ON (a.gift_type=d.id) '+
+                  'LEFT JOIN w_game_prop e ON (a.game_prop_id=e.id) '+
+                    'WHERE '+
+                      'b.id IS NOT NULL AND c.id IS NOT NULL AND d.id IS NOT NULL AND e.id IS NOT NULL '+
+                        'ORDER BY a.create_time DESC';
 
   exports.findAll = function(user_id, cb){
     mysql.query(sql, [user_id || 0], cb);
