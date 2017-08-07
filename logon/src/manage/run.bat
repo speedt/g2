a7a5f4369b71418e0e81fc2b58eba95d5f41a799
3203317 <3203317@qq.com>
2017-08-07 14:27:23 +0800

    haha

--- logon/src/manage/run.bat
+++ logon/src/manage/run.bat
@@ -1,1 +1,1 @@
-del logs\*.log /q & npm install & npm install ..\db & npm install ..\biz & node app.js
+del logs\*.log /q & npm install & npm install ..\db & npm install ..\cfg & npm install ..\model & npm install ..\biz & node app.js
