c8db2d03c61ed5f20a4455a57f0116766668e373
3203317 <3203317@qq.com>
2017-08-12 12:46:29 +0800

    haha

--- logon/src/handle/channel.js
+++ logon/src/handle/channel.js
@@ -17,33 +17,52 @@ const logger = log4js.getLogger('handle');
 
 const _ = require('underscore');
 
-exports.open = function(send, msg){
-  if(!_.isString(msg.body)) return logger.error('channel open empty');
-
-  var s = msg.body.split('::');
-
-  var server_id  = s[0];
-  var channel_id = s[1];
-
-  var send_data = [channel_id, JSON.stringify([conf.app.ver, 1, , _.now()])];
-
-  send('/queue/back.send.v3.'+ server_id, { priority: 9 }, send_data, (err, code) => {
-    if(err) return logger.error('channel open:', err);
-
-    biz.user.getUserId(server_id, channel_id, function (err, user_id){
-      if(err) return logger.error('channel open:', err);
-      if(!user_id) return logger.error('channel open:', new Error('Not Found'));
-
-      biz.user_log.saveNew({
-        log_type: 1,
-        user_id: user_id
-      }, function (err, status){
-        if(err) return logger.error('channel open:', err);
+(() => {
+  function step1(server_id, channel_id){
+    return new Promise((resolve, reject) => {
+      biz.user.getUserId(server_id, channel_id, function (err, user_id){
+        if(err) return reject(err);
+        if(!user_id) return reject(new Error('Not Found'));
+        resolve(user_id);
       });
     });
+  }
 
-  });
-};
+  function step2(user_id){
+    return new Promise((resolve, reject) => {
+        biz.user_log.saveNew({
+          log_type: 1,
+          user_id: user_id
+        }, function (err, status){
+          if(err) return reject(err);
+          resolve();
+        });
+    });
+  }
+
+  exports.open = function(send, msg){
+    if(!_.isString(msg.body)) return logger.error('channel open empty');
+
+    var s = msg.body.split('::');
+
+    var server_id  = s[0];
+    var channel_id = s[1];
+
+    var send_data = [channel_id, JSON.stringify([conf.app.ver, 1, , _.now()])];
+
+    send('/queue/back.send.v3.'+ server_id, { priority: 9 }, send_data, (err, code) => {
+      if(err) return logger.error('channel open:', err);
+
+      step1.call(null, server_id, channel_id).then(user_id => {
+        return step2.call(null, user_id);
+      }).then(function(){
+      }).catch(err => {
+        logger.error('channel open:', err);
+      });
+
+    });
+  };
+})();
 
 exports.close = function(send, msg){
   if(!_.isString(msg.body)) return logger.error('channel close empty');
