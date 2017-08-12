49ec30ffddea75c6efd70de3a782a5d1e655893a
3203317 <3203317@qq.com>
2017-08-12 10:06:03 +0800

    haha

--- logon/src/manage/run.sh
+++ logon/src/manage/run.sh
@@ -1,1 +1,11 @@
-npm install && npm install ../db/ && npm install ../lib/ && npm install ../cfg/ && npm install ../model/ && npm install ../biz/ && export ACTIVEMQ_PORT=61613 && export MYSQL_PORT=3306 && export MYSQL_PASS=123456 && export REDIS_PORT=6379 && pm2 start app.js --name manage 
+npm install \
+&& npm install ../db/ \
+&& npm install ../lib/ \
+&& npm install ../cfg/ \
+&& npm install ../model/ \
+&& npm install ../biz/ \
+&& export ACTIVEMQ_PORT=61613 \
+&& export MYSQL_PORT=3306 \
+&& export MYSQL_PASS=123456 \
+&& export REDIS_PORT=6379 \
+&& pm2 start app.js --name manage
