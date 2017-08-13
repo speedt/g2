782c7d046383a25db1650090a5e9115c78930a2b
3203317 <3203317@qq.com>
2017-08-13 10:09:32 +0800

    haha

--- logon/src/backend/settings.js
+++ logon/src/backend/settings.js
@@ -13,22 +13,22 @@ module.exports = {
     resHost: '127.0.0.1',
   },
   activemq: {
-    host: '127.0.0.1',
+    host: process.env.ACTIVEMQ_HOST || '127.0.0.1',
     port: process.env.ACTIVEMQ_PORT || 12613,
     user: 'admin',
     password: process.env.ACTIVEMQ_PASS || 'admin',
   },
   mysql: {
     database: 'emag2',
-    host: '127.0.0.1',
+    host: process.env.MYSQL_HOST || '127.0.0.1',
     port: process.env.MYSQL_PORT || 12306,
     user: 'root',
     password: process.env.MYSQL_PASS || 'password',
     connectionLimit: 50
   },
   redis: {
+    host: process.env.REDIS_HOST || '127.0.0.1',
     port: process.env.REDIS_PORT || 12379,
-    host: '127.0.0.1',
     password: process.env.REDIS_PASS || '123456',
     database: 1
   }
