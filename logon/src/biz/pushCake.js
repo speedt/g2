f3deed0b4f65a7d2b7e4ccf4991b46964efb8222
speedt <huangxin@foreworld.net>
2017-08-27 13:04:06 +0800

    haha

--- logon/src/biz/pushCake.js
+++ logon/src/biz/pushCake.js
@@ -51,8 +51,8 @@ const logger = require('log4js').getLogger('biz.pushCake');
 
   function p3(trans, group){
     return new Promise((resolve, reject) => {
-      if(3 > group.group_user_seat_sum_ready) return resolve();
       if(0 < group.status) return reject('游戏已经开始');
+      if(3 > group.group_user_seat_sum_ready) return resolve();
       biz.group.editReady(group.id, trans)
       .then(() => resolve())
       .catch(reject);
