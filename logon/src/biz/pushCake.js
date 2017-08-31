02772e45d7ca027c1dc956b4322f1a45d272b132
speedt <huangxin@foreworld.net>
2017-08-31 17:59:15 +0800

    hah

--- logon/src/biz/pushCake.js
+++ logon/src/biz/pushCake.js
@@ -34,7 +34,7 @@ const logger = require('log4js').getLogger('biz.pushCake');
 
     var ready_count = room.ready(user.id);
 
-    if(3 < ready_count) return Promise.resolve(user);
+    if(!(3 < ready_count)) return Promise.resolve(user);
 
     return Promise.resolve(user);
   }
@@ -50,96 +50,5 @@ const logger = require('log4js').getLogger('biz.pushCake');
       .then(user => resolve(user))
       .catch(reject);
     });
-  };
-
-  function p6(next, group){
-    setTimeout(() => {
-      biz.group_craps.saveNew({
-        group_id:  group.group_id,
-        round_id:  group.group_round_id,
-        round_pno: group.group_curr_round_pno,
-        round_no:  group.group_curr_round_no,
-        user_id:   group.user_id,
-        user_seat: group.group_curr_user_seat,
-        is_auto:   1,
-      })
-      .then(p7.bind(null, next))
-      .catch(next);
-    }, 5000);
-  }
-
-  function p7(next, group_craps_info){
-    next(null, group_craps_info);
-  }
-})();
-
-(() => {
-  function p1(user){
-    return new Promise((resolve, reject) => {
-      if(user.group_user_seat !== user.group_curr_user_seat) return reject('还没轮到我做动作');
-      if(1 !== user.group_curr_act) return reject('不能摇骰子');
-
-      mysql.beginTransaction()
-      .then(p2.bind(null, user))
-      .then(() => resolve(user))
-      .catch(reject);
-    });
-  }
-
-  function p2(user, trans){
-    return new Promise((resolve, reject) => {
-      biz.group_craps.saveNew({
-        group_id:  user.group_id,
-        round_id:  user.group_round_id,
-        round_pno: user.group_curr_round_pno,
-        round_no:  user.group_curr_round_no,
-        user_id:   user.user_id,
-        user_seat: user.group_curr_user_seat,
-        is_auto:   0,
-      }, trans)
-      .then(p4.bind(null, user, trans))
-      .then(mysql.commitTransaction.bind(null, trans))
-      .then(() => resolve())
-      .catch(p3.bind(null, reject, trans));
-    });
-  }
-
-  function p3(reject, trans, err){
-    trans.rollback(() => reject(err));
-  }
-
-  function p4(user, trans, group_craps_info){
-    if(4 > user.group_curr_user_seat){
-      return biz.group.editNextCraps({
-        id: user.group_id,
-        extend_curr_user_seat: user.group_curr_user_seat + 1,
-      }, trans);
-    }
-
-    return Promise.resolve();
-  }
-
-  /**
-   * 骰子确定谁是庄
-   * 1、如果是1、2、3，则通知下一个人摇骰子并显示当前人的骰子点数
-   * 2、如果是4，则算出此次哪个人（庄）的骰子点数最大，并让这个人再摇骰子，来确定哪个人先起牌
-   *
-   * @return
-   */
-  exports.crapsBanker = function(server_id, channel_id){
-    return new Promise((resolve, reject) => {
-      biz.user.getByChannelIdV2(server_id, channel_id)
-      .then(p1)
-      .catch(reject);
-    });
-  };
-})();
-
-(() => {
-  /**
-   *
-   * @return
-   */
-  exports.safeCheck = function(){
   };
 })();
--- logon/src/handle/pushCake.js
+++ logon/src/handle/pushCake.js
@@ -16,20 +16,26 @@ const logger = require('log4js').getLogger('handle.pushCake');
 
 const _ = require('underscore');
 
+const roomPool = require('emag.model').roomPool;
+
 (() => {
-  function p1(send, data, result){
-    if(0 === result.length) return;
+  function p1(send, data, user){
+    if(!user) return;
+
+    var room = roomPool.get(user.group_id);
+    if(!room) return;
+    if(0 === _.size(room.users)) return;
 
     var _data = [];
     _data.push(null);
-    _data.push(JSON.stringify([5006, data.seqId, _.now(), result[1]]));
+    _data.push(JSON.stringify([5006, data.seqId, _.now(), user]));
 
-    for(let i of result[0]){
+    for(let i of _.values(room.users)){
       if(!i.server_id || !i.channel_id) continue;
       _data.splice(0, 1, i.channel_id);
 
       send('/queue/back.send.v3.'+ i.server_id, { priority: 9 }, _data, (err, code) => {
-        if(err) return logger.error('pushCake ready:', err);
+        if(err) return logger.error('group entry:', err);
       });
     }
   }
@@ -55,85 +61,8 @@ const _ = require('underscore');
     try{ var data = JSON.parse(msg.body);
     }catch(ex){ return; }
 
-    biz.pushCake.ready(data.serverId, data.channelId, next.bind(null, send, data))
+    biz.pushCake.ready(data.serverId, data.channelId)
     .then(p1.bind(null, send, data))
     .catch(p2.bind(null, send, data));
   };
-
-  /**
-   *
-   */
-  function next(send, data, err, result){
-    if(err) return p3(send, data, err);
-    p4(send, data, result);
-  }
-
-  function p3(send, data, err){
-    logger.error('pushCake ready next:', err);
-  }
-
-  function p4(send, data, result){
-    console.log(result);
-  }
-})();
-
-(() => {
-  function p1(send, data, result){
-    if(0 === result.length) return;
-
-    var _data = [];
-    _data.push(null);
-    _data.push(JSON.stringify([5012, data.seqId, _.now(), result[1]]));
-
-    for(let i of result[0]){
-      if(!i.server_id || !i.channel_id) continue;
-      _data.splice(0, 1, i.channel_id);
-
-      send('/queue/back.send.v3.'+ i.server_id, { priority: 9 }, _data, (err, code) => {
-        if(err) return logger.error('pushCake craps:', err);
-      });
-    }
-  }
-
-  function p2(send, data, err){
-    if('string' !== typeof err) return logger.error('pushCake craps:', err);
-
-    var _data = [];
-    _data.push(data.channelId);
-    _data.push(JSON.stringify([5012, data.seqId, _.now(), , err]));
-
-    send('/queue/back.send.v3.'+ data.serverId, { priority: 9 }, _data, (err, code) => {
-      if(err) return logger.error('pushCake craps:', err);
-    });
-  }
-
-  /**
-   *
-   */
-  exports.craps = function(send, msg){
-    if(!_.isString(msg.body)) return logger.error('pushCake craps empty');
-
-    try{ var data = JSON.parse(msg.body);
-    }catch(ex){ return; }
-
-    biz.pushCake.craps(data.serverId, data.channelId, next.bind(null, send, data))
-    .then(p1.bind(null, send, data))
-    .catch(p2.bind(null, send, data));
-  };
-
-  /**
-   *
-   */
-  function next(send, data, err, result){
-    if(err) return p3(send, data, err);
-    p4(send, data, result);
-  }
-
-  function p3(send, data, err){
-    logger.error('pushCake craps next:', err);
-  }
-
-  function p4(send, data, result){
-    console.log(result);
-  }
 })();
